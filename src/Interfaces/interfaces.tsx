870f6a37e065c36e2f40c066fb414328ea71db48
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-07-19 13:41:26 -0400

    cambios viernes

---
+++ src/Interfaces/interfaces.tsx
@@ -0,0 +1,11 @@
+export interface Usuario {
+    nombre: string;
+    apellido: string;
+    rut: string;
+    telefonocasa: number;
+    celular: number;
+    edad: number;
+    correo: string;
+    fechanacimiento: string;
+  }
+  
