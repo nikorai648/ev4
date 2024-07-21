d7b4e47404af508d78f1134b4f111d005f7aa53f
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-07-20 20:13:28 -0400

    Update index.tsx

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app'; // Asegúrate de que la ruta sea correcta
+import App from './app'; 
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
