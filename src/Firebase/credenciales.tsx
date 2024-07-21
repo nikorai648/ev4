aad663eedf1b50b1380c6440c39bc283bd4d1c59
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-07-21 17:43:08 -0400

    cambios domingo

--- src/Firebase/credenciales.tsx
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCsiKaHbFkmkvJv_TYZ4M2VZrwK48F-0jI",
-    authDomain: "ev4fe-aa0b7.firebaseapp.com",
-    projectId: "ev4fe-aa0b7",
-    storageBucket: "ev4fe-aa0b7.appspot.com",
-    messagingSenderId: "71394017342",
-    appId: "1:71394017342:web:f7bf97bbbbf41cfb08916f",
-    measurementId: "G-5WSEVML78G"
-};
