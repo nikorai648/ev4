501724332b709e47c28c7abf977487edd13354e7
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-07-20 21:00:00 -0400

    cambios sabado

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 // src/index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../app';
+import App from './app';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
